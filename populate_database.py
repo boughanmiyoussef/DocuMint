81f701b075843686ac8bb5f9bdae299af3df4e2b
Youssef <yussefboughanmy@gmail.com>
2026-08-07 15:03:57 +0100

    define load documents function

--- populate_database.py
+++ populate_database.py
@@ -1,10 +1,34 @@
+import argparse
 import os
 import shutil
-import argparse
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 from langchain_community.vectorstores import Chroma
 from get_embedding_function import get_embedding_function
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+    
+    if args.reset:
+        print("Clearing Database")
+        clear_database()
+        
+        
+    
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+    
+    
+    
+def load_documents():
+    documents_loader = PyPDFDirectoryLoader(DATA_PATH)
+    return documents_loader.load()
+
+
