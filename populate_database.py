0d4cdabf3f82cf8dd2313dc4f4fa4526ea8a3ac1
Youssef <yussefboughanmy@gmail.com>
2026-08-07 15:30:10 +0100

    add the split function

--- populate_database.py
+++ populate_database.py
@@ -32,3 +32,12 @@ def load_documents():
     return documents_loader.load()
 
 
+def split_documents(documents):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function = len,
+        is_seperator_regex = False
+        )
+
+    return text_splitter.split_documents(documents)
