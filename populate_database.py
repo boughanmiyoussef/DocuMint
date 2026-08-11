cf135e4bcf88589067331ffd877a6f112c2561f2
Youssef <yussefboughanmy@gmail.com>
2026-08-11 19:30:03 +0100

    populate the db

--- populate_database.py
+++ populate_database.py
@@ -69,3 +69,24 @@ def add_to_chroma(chunks):
         db.persist()
     else:
         print("No new documents to add")
+        
+        
+def calculate_chunk_ids(chunks):
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+        chunk.metadata["id"] = chunk_id
+
+    return chunks
