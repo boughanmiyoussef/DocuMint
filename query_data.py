24e0425c4e3e11a8f6742cd6580f5d249d377a78
Youssef <yussefboughanmy@gmail.com>
2026-08-21 07:05:33 +0100

    add RAG query script using Chroma and Ollama

---
+++ query_data.py
@@ -0,0 +1,46 @@
+import argparse
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import Chroma
+from get_embedding_function import get_embedding_function
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+def query_rag(query_text: str, model_name: str = "phi3:mini"):
+    embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    results = db.similarity_search_with_score(query_text, k=5)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+
+    model = Ollama(model=model_name)
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"response: {response_text}\nsources: {sources}"
+    print(formatted_response)
+    return response_text
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="the query text.")
+    parser.add_argument("--model", type=str, default="phi3:mini", help="Ollama model to use")
+    args = parser.parse_args()
+    query_text = args.query_text
+    print(query_rag(query_text, args.model))
+
+if __name__ == "__main__":
+    main()
